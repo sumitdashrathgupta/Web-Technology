d353e66dd624054d73925f6e03f194f019f01127
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2024-08-16 14:15:26 +0530

    update

--- Javascript/#1/script.js
+++ Javascript/#1/script.js
@@ -19,3 +19,23 @@ console.log(typeof d);//object
 console.log(typeof e);//undefined
 console.log(typeof f);//bigint
 console.log(typeof g);//symbol
+
+
+
+// This is a non primitive data type
+
+var object = {
+    name: 'sumit',
+    roll_no: 124,
+    class: 'bsc IT'
+}
+console.log(object);
+console.log(object.class);
+
+
+let array_ex=[12,34,'sumit',43535n,true,Symbol(12),null]
+console.log(array_ex);
+
+for (const element of array_ex) {
+    console.log(element," = ",typeof element);
+}
