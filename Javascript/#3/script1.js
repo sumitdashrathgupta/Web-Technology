9cd677109eabd063a3c55db64ffeca71e5fcf0b9
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2024-08-16 20:18:32 +0530

    update

--- Javascript/#3/script1.js
+++ Javascript/#3/script1.js
@@ -1,1 +1,10 @@
-console.log(z);
+console.log(10+"20");
+console.log("java"+"script");
+console.log("java"-"script");
+console.log(9-"5");
+console.log(""+"");
+console.log(""+0);
+console.log(true+true);
+console.log(true+false);
+console.log(false+true);
+console.log(false-true);
