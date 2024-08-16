1d9e65ea8572ad1c1461ff29a86c2e861a5ef1a5
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2024-08-16 18:57:15 +0530

    update

---
+++ Javascript/#3/script.js
@@ -0,0 +1,22 @@
+// let a=20
+// console.log(a);
+// function a1(b,c){
+//     console.log("sumit " + b);
+//     console.log("gupta " + c);
+//     console.log(b+c);
+//     console.log(b-c);
+//     console.log(b*c);
+// }
+
+// a1(2,3)
+// a1(2,2)
+
+
+function add(x,y)
+{
+      console.log(x+y);
+     return x+y;
+}
+
+const addition= add(12,2)
+console.log(addition);
