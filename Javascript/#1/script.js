e6b127f9cd37189b8596cfd847be12ce20d097b3
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2024-08-16 14:08:52 +0530

    update

--- Javascript/#1/script.js
+++ Javascript/#1/script.js
@@ -1,17 +1,21 @@
 console.log("Hello world");
 
-var a=12
-var b="sumit"
-var c=true
-var d=null
-var e
-var f=1233n
-var g=Symbol(12)
 
-console.log( typeof a);//number
-console.log( typeof b);//String
-console.log( typeof c);//boolean
-console.log( typeof d);//object
-console.log( typeof e);//undefined
-console.log( typeof f);//bigint
-console.log( typeof g);//symbol
+
+//This is a datd type= primitive data type
+
+var a = 12
+var b = "sumit"
+var c = true
+var d = null
+var e
+var f = 1233n
+var g = Symbol(12)
+
+console.log(typeof a);//number
+console.log(typeof b);//String
+console.log(typeof c);//boolean
+console.log(typeof d);//object
+console.log(typeof e);//undefined
+console.log(typeof f);//bigint
+console.log(typeof g);//symbol
