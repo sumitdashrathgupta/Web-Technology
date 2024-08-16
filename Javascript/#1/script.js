8ed0da5e5f74931641b5c40d325c774bcc9c5d01
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2024-08-16 14:07:30 +0530

    update

--- Javascript/#1/script.js
+++ Javascript/#1/script.js
@@ -1,1 +1,17 @@
 console.log("Hello world");
+
+var a=12
+var b="sumit"
+var c=true
+var d=null
+var e
+var f=1233n
+var g=Symbol(12)
+
+console.log( typeof a);//number
+console.log( typeof b);//String
+console.log( typeof c);//boolean
+console.log( typeof d);//object
+console.log( typeof e);//undefined
+console.log( typeof f);//bigint
+console.log( typeof g);//symbol
