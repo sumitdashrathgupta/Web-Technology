e7ae4c721a3b8f34f294b22f08886b9f251b7533
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2024-09-05 16:58:21 +0530

    update

---
+++ Javascript/#12/index.js
@@ -0,0 +1,18 @@
+// console.log("hello wolrd");
+// function add(a, b) {
+//     console.log(a + b);
+//     mul(2,4);
+// }
+
+//  function mul(a, b) {
+//     console.log(a*b);
+// }
+
+// add(2, 2)
+
+let addition = add = (a, b) => {
+    return a + b
+}
+let ok = add(2, 3)
+console.log(ok);
+console.log(addition);
---
+++ Javascript/#13/script.js
@@ -0,0 +1,45 @@
+//replace()
+
+/*let str="this is a javascript tutorial"
+let sys=str.replace("javascript","react")
+console.log(sys);
+console.log(str);*/
+
+//replaceAll
+
+/*let str="this is a javascript tutorial"
+let sys=str.replaceAll("javascript","react")
+console.log(sys);
+console.log(str);
+*/
+
+//trim()
+
+// let str="                        this is a javascript tutorial"
+// let sys=str.trim()
+// console.log(sys);
+// console.log(str);
+
+// String method - split() => conver to string to array
+
+let a = "sumit"
+// let b = a.split("")
+// console.log(a);
+// console.log(b);
+// console.log(a.split("d"));
+
+
+
+// array method  - join() => convert array to string
+
+
+//reverse
+// let x= a.split("").reverse().join("");
+// console.log(x);
+
+// let y=a.split("").reduceRight((acc,cv)=>acc+cv,"")
+// console.log(y);
+
+
+
+
