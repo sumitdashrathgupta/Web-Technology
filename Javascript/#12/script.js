a0de0536bffc5405a3cc4569a1b16b9660473a24
sumitdashrathgupta <sumitdashrathgupta111@gmai.com>
2024-09-09 18:57:57 +0530

    update

---
+++ Javascript/#12/script.js
@@ -0,0 +1,23 @@
+// function intro(name, work) {
+//        return console.log(`Hello my is ${name} & I am ${work} engneering`)
+//     return `Hello my is ${name} & I am ${work} engneering`;
+// }
+
+//  let ok =intro('sumit','terader')
+//  console.log(ok)
+// intro('Amit','software')
+// intro('Raj','testing')
+// let sys=intro('Ram','web developer')
+// console.log( typeof intro)
+
+
+// function add(x, y) {
+//     return x * y;
+// }
+
+// let a = prompt("Enter the first num")
+// let b = prompt("Enter the Second num")
+// let sum = add(a, b)
+// alert(`This is a value ${sum}`)
+
+
---
+++ Javascript/OOP/index.js
@@ -0,0 +1,27 @@
+// console.log("hello world")
+
+//Encapsulation
+const onj = {
+    name: "sumit",
+    rollno: 264,
+    mettre: false,
+    age: 21,
+    userage: function () {
+        return new Date().getFullYear() - onj.age
+    }
+}
+
+console.log(onj.userage())
+
+
+//Abstraction
+
+
+
+//Inheritance
+
+
+
+
+
+//Polymorphism
