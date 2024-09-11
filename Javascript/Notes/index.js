cd8d4ae523f15d960746334a15c5e427a1b6c78d
sumitdashrathgupta <sumitdashrathgupta111@gmai.com>
2024-09-11 10:56:19 +0530

    update

---
+++ Javascript/Notes/index.js
@@ -0,0 +1,37 @@
+// let a=[23,45,66,89,434]
+// console.log(a.at(-2))
+
+let b="sumit"
+
+// let x=b.split("").reverse()
+// let y=x.join()
+// console.log()
+// console.log(b.slice(2,4))
+// console.log(b.at(-1))
+// let x=a.reverse()
+// console.log(x)
+
+// let sys=a.sort((a,b)=>{
+//     if(a>b)return 1
+//     else if(a<b) return -1
+// })
+// console.log(sys)
+
+// const productPrice = [100, 200, 300, 400, 500];
+
+// const totalPrice = productPrice.reduce((accum, curElem) => {
+//   return accum + curElem ;
+// }, 0);
+
+// console.log(totalPrice);
+
+
+// function reverseString(str) {
+//     if (str === "") {
+//         return "";
+//     } else {
+//         return reverseString(str.substr(1)) + str.charAt(0);
+//     }
+// }
+
+// console.log(reverseString("hello")); // Output: "olleh"
