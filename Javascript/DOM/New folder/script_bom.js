ee9db0c92a9518cdff8baf7611d24a8b8522307e
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2024-09-16 09:38:22 +0530

    update

---
+++ Javascript/DOM/New folder/script_bom.js
@@ -0,0 +1,18 @@
+// This  is a BOM
+
+// alert("hello")
+// let a=confirm("Yes or no")
+// console.log(a);
+// let a=window.prompt("Enter the number ")
+// console.log(a);
+// let a=console.warn("hey");
+// let b=console.error("hey");
+
+// This is a DOM
+
+// document.writeln("world")
+// document.write("Hello")
+
+
+
+alert("wlecome")
