d28ee41dc646d21e60f05eb41a33cf2762365f6a
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2024-09-26 21:19:10 +0530

    today

---
+++ Javascript/26-09-24/script.js
@@ -0,0 +1,21 @@
+// Fetch method
+let data = window.fetch("");
+data.then((d) => {
+    let p1 = d.json();
+    p1.then(({ products: v }) => {
+        let tbody = document.querySelector("tbody");
+        console.log(tbody);
+        console.log(v);
+        v.map((value, index) => {
+            let { id, title, image, description, price, thumbnail } = value;
+            console.log(value);
+            tbody.innerHTML += `<tr>
+            <td>${id}</<td>
+            <td><img src=""${image || thumbnail}/></<td>
+            <td>${title}</<td>
+            <td>${description}</<td>
+            <td>${price}</td>
+            </tr > `;
+        });
+    }, (r) =>r);
+}, (e) => e);
