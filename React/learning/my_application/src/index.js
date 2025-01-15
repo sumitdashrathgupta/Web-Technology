a09045de8608a302bdc2fdc2a90d9ad0657dcf51
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2025-01-15 23:07:43 +0530

    update

--- React/learning/my_application/src/index.js
+++ React/learning/my_application/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client"
 import "./CLC/Fetch API/Style.css"
-import App from "./Context API/Form Submit/App"
+import App from "./Context API/File Props/App"
 createRoot(document.querySelector("#root")).render(<App />)
