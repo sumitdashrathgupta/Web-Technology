b80bb8916e0d85aae84969586df88f95bc4968f4
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2025-02-04 21:09:01 +0530

    update

---
+++ React/learning/my_application/src/index1.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { createRoot } from "react-dom/client"
+import "./CLC/Fetch API/Style.css"
+// import App from "./Context API/File Props/App"
+// createRoot(document.querySelector("#root")).render(<App />)
+<<<<<<< HEAD
+
+// import { BrowserRouter } from "react-router-dom";
+import App from "./State/App2"
+=======
+// import App from "./Router/App"
+// import { BrowserRouter } from "react-router-dom";
+import App from "./Form Data/App"
+>>>>>>> ab77eef0c73e413bc1515df7d9da3971f91000c3
+
+createRoot(document.querySelector("#root")).render(
+    // <BrowserRouter>
+        <App />
+    // </BrowserRouter>
+
+)
+
