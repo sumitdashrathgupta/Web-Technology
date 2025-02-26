4f2fe92d5bff6882c522e6a039bde9208b07418e
sumitdashrathgupta <sumitdashrathgupta111@gmai.com>
2025-02-26 23:06:58 +0530

    update

--- Project/TTT/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './Components/Style/TTT.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-    <App />
-)
