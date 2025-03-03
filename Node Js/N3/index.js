fb9b4468ef6c085ce430fdbe2bc0aceb7caaa6f1
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2025-03-03 15:39:01 +0530

    update n4

--- Node Js/N3/index.js
+++ Node Js/N3/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("Hello world")
@@ -9,5 +10,30 @@ app.get("/", (req, res) => {
 app.post("/news", (req, res) => {
     res.send("This is a news")
 })
+
+
+//!body in json
+
+// app.post("/login", (req, res) => {
+//     console.log(req.body);
+//     res.status(200).send(req.body)
+//     // res.send("This is a login", req.body)
+// })
+
+//! This is a query
+
+// app.post("/login", (req, res) => {
+//     console.log(req.query);
+//     res.status(200).send(req.query)
+//     // res.send("This is a login", req.body)
+// })
+
+//! This is a params
+app.post("/login/:id", (req, res) => {
+    console.log(req.params.id);
+    res.status(200).send(req.params)
+    // res.send("This is a login", req.body)
+})
+
 
 app.listen("4000")
