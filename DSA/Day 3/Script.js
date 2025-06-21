ee331997231e9e74bb2e882d70c8c6f741c342a1
sumitdashrathgupta <sumitdashrathgupta111@gmai.com>
2025-06-21 12:53:05 +0530

    change all file

--- DSA/Day 3/Script.js
+++
@@ -1,7 +0,0 @@
-let value = Number(prompt("Enter the number"));
-
-if (value == 0 || value == 1) {
-  console.log("1");
-} else {
-  console.log(value * (value - 1));
-}
--- Javascript Project/Interview/Q2.js
+++
@@ -1,9 +0,0 @@
-// Reverse this string
-
-const word = "hello Javascript to typescript";
-const arry = word.split(" ");
-const value = arry.map((word) => {
-  return word.split(" ").reverse().join(" ");
-});
-
-console.log(value.join(" "));
